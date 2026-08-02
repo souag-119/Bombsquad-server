4bdade79abca335429db219e87479f94500b91c7
souag-119 <abdenoursouag8@gmail.com>
2026-08-02 23:19:34 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,6 @@
-# اسم السيرفر الذي سيظهر للاعبين في القائمة العامة
-config['party_name'] = 'titabronsia server سيرفر عربي'
-
-# جعل السيرفر عاماً ليظهر في قائمة Internet باللعبة
-config['party_is_public'] = True
-
-# الحد الأقصى للاعبين
-config['max_party_size'] = 10
-
-# المنفذ
-config['port'] = 43210
+# config.py
+party_name = "tita server سيرفر عربي"
+max_party_size = 10
+party_is_public = True  # إظهار السيرفر في قائمة الأونلاين للأندرويد
+session_type = "free_for_all"  # أو "teams"
+port = 43210
