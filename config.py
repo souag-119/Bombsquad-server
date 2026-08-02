a5f66f52a40d9f1776bd53df57e9e4bef8339997
souag-119 <abdenoursouag8@gmail.com>
2026-08-03 00:06:50 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,6 @@
-# config.py - BombSquad Server Config
-
-# اسم السيرفر
-party_name = "Android Public Server - Render"
-
-# أقصى عدد لاعبين
+# config.py
+party_name = "Android Fly.io Public Server"
 max_party_size = 8
-
-# جعل السيرفر علنياً في القائمة العامة للأندرويد
 party_is_public = True
-
-# المنفذ الافتراضي
-port = 43210
-
-# نوع الجلسة
 session_type = "free_for_all"
+port = 43210
