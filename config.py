6fe7034d6e961f708dc0aa0ec1b56462554dde97
souag-119 <abdenoursouag8@gmail.com>
2026-08-02 20:07:04 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# اسم السيرفر الذي سيظهر للاعبين في القائمة العامة
+config['party_name'] = 'titabronsia server سيرفر عربي'
+
+# جعل السيرفر عاماً ليظهر في قائمة Internet باللعبة
+config['party_is_public'] = True
+
+# الحد الأقصى للاعبين
+config['max_party_size'] = 10
+
+# المنفذ
+config['port'] = 43210
